9c4c52d14b0dccaf74973600910330202a45ae44
mac669 <mac.estrada.xr@gmail.com>
2021-07-05 13:35:22 -0500

    blog con css bulma 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   #get 'home/index'
-  root "home#index"
+
+   root "home#index"
+  #get 'home', to: "home#index"
+
+  #root "home#pruebas"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
